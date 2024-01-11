e44a0a2d597bad7b3bbe8fcd947f514f62ebcb19
tiagombp <tiagombp@gmail.com>
2024-01-11 01:35:44 +0100

    some ad-hoc plots for the presentation

--- data-prep-implementation.R
+++ data-prep-implementation.R
@@ -3,6 +3,8 @@ library(dbscan)
 library(dslabs)
 library(Rtsne)
 library(colorspace)
+library(extrafont)
+loadfonts()
 
 datagap <- dslabs::gapminder %>% filter(
   year == 2011) %>%
@@ -12,6 +14,10 @@ datagap <- dslabs::gapminder %>% filter(
     log_gdp = log10(gdp)
   ) %>%
   unique()
+
+datagap_num <- datagap %>%
+  mutate(log_pop = log10(population)) %>%
+  select(-country, -year, -continent, -region, -gdp, -gdp_million, -population)
 
 skimr::skim(datagap)
 
@@ -58,9 +64,22 @@ ggplot(datagap_vis, aes(x = x, y = y)) +
   theme_minimal()
 
 
+# Plots for the Presentation ----------------------------------------------
+
 ggplot(datagap, aes(x = gdp_million, y = life_expectancy, size = population, color = continent)) + 
   geom_point() +
   scale_x_log10(breaks = c(1e3, 1e5, 1e7), labels = c("US$ 1 billion", "US$ 100 billion", "US$ 10 trillion")) + #scales::number_format(big.mark = ".")) +
   scale_color_discrete_qualitative(palette = "Set 2") +
-  labs(x = "GDP", y = "Life Expectancy") +
-  theme_minimal()
+  scale_size(breaks = c(1e6, 1e8, 1e9), labels = c("1 million", "100 million", "1 billion")) +
+  labs(x = "GDP", y = "Life Expectancy", size = "Pop", color = "Continent") +
+  theme_minimal() +
+  theme(
+    text = element_text(family = "Fira Code")
+  )
+
+ggsave("gapminder-bubble.png")
+
+library(GGally)
+
+ggpairs(datagap_num)
+ggsave("gapminder-scatterplot-matrix.png")
